ead0ce8796885738fa1debd8263d870cabbdf606
allester <allester@ramayrat.com>
2024-01-12 09:13:16 -0800

    cli client

--- src/client/main.rs
+++ src/client/main.rs
@@ -3,10 +3,6 @@ use clap::{Parser, Subcommand};
 
 use authentication::auth_client::AuthClient;
 use authentication::{SignInRequest, SignOutRequest, SignUpRequest};
-use tonic::transport::Channel;
-use tonic::{Request, Response};
-
-use crate::authentication::{SignUpResponse, SignInResponse, SignOutResponse};
 
 pub mod authentication {
     tonic::include_proto!("authentication");
@@ -41,32 +37,38 @@ enum Commands {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // AUTH_SERVICE_IP can be set to your droplet's ip address once your app is deployed
     let auth_ip = env::var("AUTH_SERVICE_IP").unwrap_or("[::0]".to_owned());
-    let mut client: AuthClient<Channel> = todo!(); // Create new `AuthClient` instance. Propagate any errors.
+    let mut client = AuthClient::connect(format!("http://{}:50051", auth_ip)).await?;
 
     let cli = Cli::parse();
 
     match &cli.command {
         Some(Commands::SignIn { username, password }) => {
-            let request: Request<SignInRequest> = todo!(); // Create a new `SignInRequest`.
+            let request = tonic::Request::new(SignInRequest {
+                username: username.clone(),
+                password: password.clone(),
+            });
         
-            // Make a sign in request. Propagate any errors. Convert Response<SignInResponse> into SignInResponse.
-            let response: SignInResponse = todo!();
+            let response = client.sign_in(request).await?.into_inner();
         
             println!("{:?}", response);
         }
         Some(Commands::SignUp { username, password }) => {
-            let request: Request<SignUpRequest> = todo!(); // Create a new `SignUpRequest`.
+            let request = tonic::Request::new(SignUpRequest {
+                username: username.clone(),
+                password: password.clone(),
+            });
         
-            let response: Response<SignUpResponse> = todo!(); // Make a sign up request. Propagate any errors.
+            let response = client.sign_up(request).await?;
         
             println!("{:?}", response.into_inner());
         }
         Some(Commands::SignOut { session_token }) => {
-            let request: Request<SignOutRequest> = todo!(); // Create a new `SignOutRequest`.
+            let request = tonic::Request::new(SignOutRequest {
+                session_token: session_token.clone(),
+            });
         
-            let response: Response<SignOutResponse> = todo!(); // Make a sign out request. Propagate any errors.
+            let response = client.sign_out(request).await?;
         
             println!("{:?}", response.into_inner());
         }
